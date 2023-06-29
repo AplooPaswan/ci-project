e19c694fca2648c12db91f5df5f0a0bde6d06d83
aploo paswan <aploopaswan5@gmail.com>
2023-06-29 18:50:14 +0530

    3rd

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 
 // phpinfo();
 
-
 $tmp = [
     ["name"=>"Aploo","Paswan"],
     ["name"=>"Aploo","Paswan2"],
     ["name"=>"Aploo","Paswan3"],
     ["name"=>"Aploo","Paswan4"],
 ];
-
+echo "<pre>";
 print_r($tmp);
