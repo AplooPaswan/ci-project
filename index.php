396be29a9bd6071eb12a1cb9cb271f6a8b664c82
aploo paswan <aploopaswan5@gmail.com>
2023-06-29 18:44:31 +0530

    second commit

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-phpinfo();
+// phpinfo();
+
+
+$tmp = [
+    ["name"=>"Aploo","Paswan"],
+    ["name"=>"Aploo","Paswan2"],
+    ["name"=>"Aploo","Paswan3"],
+    ["name"=>"Aploo","Paswan4"],
+];
+
+print_r($tmp);
